e6bb978b957cb2d64197bee9094d71ca46c259ee
anshitmishra <anshitmishra03@gmail.com>
2022-12-19 20:04:48 +0530

    added template of code and idea ppt

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hackathon-submission-template
 Please use this template for submitting solutions for the Microsoft Future Technology Hackathon 2022
+
+# MUST VIEW PPT THIS CONTAIN MY IDEA EXPLANATION 
+# APP IS IN UNDER PROCESS 
---
+++ source/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello started microsoft project prototype under process</Text>
+      <StatusBar style="auto" />
+      <Button title='save'>save</Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
